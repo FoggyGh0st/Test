69ffb708ce478d954776c0ba56228cbd99258756
FoggyGh0st <113252795+FoggyGh0st@users.noreply.github.com>
2022-10-01 14:20:24 +0300

    Add files via upload

---
+++ calculator.php.php
@@ -0,0 +1,59 @@
+<?php
+function calculator($argv): string
+{
+    $numberarr = [];
+    $sarr = [];
+    $tempstring = '';
+    $sum = 0;
+    $fullcheck = '0123456789+-';
+    $numbercheck = '0123456789';
+    foreach (str_split($argv[1] as $char))
+    {
+        if(!(strpos($fullcheck, $char)))
+        {
+            return 'Error!';
+
+        }
+        else
+        {
+            if(strpos($numbercheck, $char))
+            {
+                $tempstring .= $char;
+            }
+            else
+            {
+                $sarr[] = $char;
+                $numberarr[] = intval($tempstring);
+                $tempstring = '';
+            }
+        }
+    }
+    $numberarr[] = intval($tempstring);
+    foreach ($sarr as $key => $value)
+    {
+        if($key === 0)
+        {
+            if ($value === '-')
+            {
+            $sum -= $numberarr[$key] - $numberarr[key-1];
+            }
+            if ($value === '+')
+            {
+            $sum -= $numberarr[$key] + $numberarr[key+1];
+            }
+        }
+        else 
+        {
+            if ($ === '+') 
+            {
+                $sum += $numberArr[$key + 1];
+            }
+            if ($value === '-') 
+            {
+                $sum -= $numberArr[$key + 1];
+            }
+        }
+    }
+    return ($);
+}
+echo calculator($argv);
