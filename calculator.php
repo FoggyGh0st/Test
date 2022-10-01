ec3a60c55d9abec14dc5d6d913966fe96c4c3a10
FoggyGh0st <113252795+FoggyGh0st@users.noreply.github.com>
2022-10-01 22:35:17 +0300

    vso ok

--- calculator.php
+++ calculator.php
@@ -8,12 +8,12 @@ function calculator($argv): string
     $fullcheck = '0123456789+-';
     $numbercheck = '0123456789';
     foreach (str_split($argv[1]) as $char) {
-        if(!(strpos($fullcheck, $char))) {
+        if (!(strpos($fullcheck, $char))) {
             return 'Error!';
-        }else {
-            if(strpos($numbercheck, $char)) {
+        } else {
+            if (strpos($numbercheck, $char)) {
                 $tempstring .= $char;
-            }else {
+            } else {
                 $sarr[] = $char;
                 $numberarr[] = intval($tempstring);
                 $tempstring = '';
@@ -22,12 +22,12 @@ function calculator($argv): string
     }
     $numberarr[] = intval($tempstring);
     foreach ($sarr as $key => $value) {
-        if($key === 0) {
+        if ($key === 0) {
             if ($value === '-') {
-            $sum -= $numberarr[$key] - $numberarr[$key-1];
+                $sum -= $numberarr[$key] - $numberarr[$key - 1];
             }
             if ($value === '+') {
-            $sum += $numberarr[$key] + $numberarr[$key+1];
+                $sum += $numberarr[$key] + $numberarr[$key + 1];
             }
         } else {
             if ($value === '+') {
