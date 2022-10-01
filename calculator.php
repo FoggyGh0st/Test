42338f21b5d1a97dec495fcbcf4751f5cda5a994
FoggyGh0st <113252795+FoggyGh0st@users.noreply.github.com>
2022-10-01 18:35:41 +0300

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -7,7 +7,7 @@ function calculator($argv): string
     $sum = 0;
     $fullcheck = '0123456789+-';
     $numbercheck = '0123456789';
-    foreach (str_split($argv[1] as $char))
+    foreach (str_split($argv[1]) as $char)
     {
         if(!(strpos($fullcheck, $char)))
         {
@@ -35,25 +35,25 @@ function calculator($argv): string
         {
             if ($value === '-')
             {
-            $sum -= $numberarr[$key] - $numberarr[key-1];
+            $sum -= $numberarr[$key] - $numberarr[$key-1];
             }
             if ($value === '+')
             {
-            $sum -= $numberarr[$key] + $numberarr[key+1];
+            $sum -= $numberarr[$key] + $numberarr[$key+1];
             }
         }
         else 
         {
-            if ($ === '+') 
+            if ($value === '+') 
             {
-                $sum += $numberArr[$key + 1];
+                $sum += $numberarr[$key + 1];
             }
             if ($value === '-') 
             {
-                $sum -= $numberArr[$key + 1];
+                $sum -= $numberarr[$key + 1];
             }
         }
     }
-    return ($);
+    return ($sum);
 }
 echo calculator($argv);
