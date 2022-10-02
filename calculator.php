ff882e08e7ded4fda5255844184c1363314d0784
FoggyGh0st <113252795+FoggyGh0st@users.noreply.github.com>
2022-10-02 14:23:20 +0300

    nice

--- calculator.php
+++ calculator.php
@@ -1,40 +1,40 @@
 <?php
-function calculator($argv): string
+function calculator( $argv): string
 {
-    $numberarr = [];
-    $sarr = [];
-    $tempstring = '';
+    $numberArr = [];
+    $sArr = [];
+    $tempString = '';
     $sum = 0;
-    $fullcheck = '0123456789+-';
-    $numbercheck = '0123456789';
+    $fullCheck = '0123456789+-';
+    $numberCheck = '0123456789';
     foreach (str_split($argv[1]) as $char) {
-        if (!(strpos($fullcheck, $char))) {
+        if (!(strpos($fullCheck, $char))) {
             return 'Error!';
         } else {
-            if (strpos($numbercheck, $char)) {
-                $tempstring .= $char;
+            if (strpos($numberCheck, $char)) {
+                $tempString .= $char;
             } else {
-                $sarr[] = $char;
-                $numberarr[] = intval($tempstring);
-                $tempstring = '';
+                $sArr[] = $char;
+                $numberArr[] = intval($tempString);
+                $tempString = '';
             }
         }
     }
-    $numberarr[] = intval($tempstring);
-    foreach ($sarr as $key => $value) {
+    $numberArr[] = intval($tempString);
+    foreach ($sArr as $key => $value) {
         if ($key === 0) {
             if ($value === '-') {
-                $sum -= $numberarr[$key] - $numberarr[$key - 1];
+                $sum -= $numberArr[$key] - $numberArr[$key - 1];
             }
             if ($value === '+') {
-                $sum += $numberarr[$key] + $numberarr[$key + 1];
+                $sum += $numberArr[$key] + $numberArr[$key + 1];
             }
         } else {
             if ($value === '+') {
-                $sum += $numberarr[$key + 1];
+                $sum += $numberArr[$key + 1];
             }
             if ($value === '-') {
-                $sum -= $numberarr[$key + 1];
+                $sum -= $numberArr[$key + 1];
             }
         }
     }
